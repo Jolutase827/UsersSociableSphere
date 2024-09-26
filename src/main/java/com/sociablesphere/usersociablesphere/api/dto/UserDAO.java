851e1824976039242f4bc8e704aa5300fa947e1e
jsug <jolutase104@gmail.com>
2024-09-26 15:46:11 +0200

    Repository Correction

--- src/main/java/com/sociablesphere/usersociablesphere/api/dto/UserDAO.java
+++ src/main/java/com/sociablesphere/usersociablesphere/api/dto/UserDAO.java
@@ -1,4 +1,24 @@
 package com.sociablesphere.usersociablesphere.api.dto;
 
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class UserDAO {
+    private UUID id;
+    private String userName;
+    private String name;
+    private String lastName;
+    private String email;
+    private String photo;
+    private String description;
+    private String password;
+    private String role;
+    private Double wallet;
+    private String apiToken;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
 }
--- src/main/java/com/sociablesphere/usersociablesphere/repository/UserRepository.java
+++ src/main/java/com/sociablesphere/usersociablesphere/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.sociablesphere.usersociablesphere.repository;
 
 import com.sociablesphere.usersociablesphere.model.User;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends R2dbcRepository<User,Long> {
+    public Mono<User> findByUserName(String string);
+    public Mono<User> findByEmail(String string);
 }
