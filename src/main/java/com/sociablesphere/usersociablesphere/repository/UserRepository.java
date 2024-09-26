fada01c27bfe0aab02d35645c8ee52ee2b37aa4c
jsug <jolutase104@gmail.com>
2024-09-26 15:31:47 +0200

    Repository Correction

--- src/main/java/com/sociablesphere/usersociablesphere/repository/UserRepository.java
+++ src/main/java/com/sociablesphere/usersociablesphere/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.sociablesphere.usersociablesphere.repository;
 
-public interface UserRepository {
+import com.sociablesphere.usersociablesphere.model.User;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+public interface UserRepository extends R2dbcRepository<User,Long> {
 }
