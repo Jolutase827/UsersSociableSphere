c2d99efc8c30f0f3b64de81d56e33318a80618f3
Peral Belenguer <aipg@gft.com>
2024-09-26 15:48:27 +0200

    Added UUID to User Entity, created method on UserServiceImpl to create a random UUID when a User is created and Fixed UserRepository changing Long id to a UUID

--- src/main/java/com/sociablesphere/usersociablesphere/repository/UserRepository.java
+++ src/main/java/com/sociablesphere/usersociablesphere/repository/UserRepository.java
@@ -3,5 +3,7 @@ package com.sociablesphere.usersociablesphere.repository;
 import com.sociablesphere.usersociablesphere.model.User;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
-public interface UserRepository extends R2dbcRepository<User,Long> {
+import java.util.UUID;
+
+public interface UserRepository extends R2dbcRepository<User, UUID> {
 }
--- src/main/java/com/sociablesphere/usersociablesphere/service/user/UserServiceImpl.java
+++ src/main/java/com/sociablesphere/usersociablesphere/service/user/UserServiceImpl.java
@@ -1,4 +1,22 @@
 package com.sociablesphere.usersociablesphere.service.user;
 
+import com.sociablesphere.usersociablesphere.model.User;
+import com.sociablesphere.usersociablesphere.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Service
 public class UserServiceImpl {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Mono<User> createUser(User user) {
+        user.setId(UUID.randomUUID());
+        return userRepository.save(user);
+    }
 }
