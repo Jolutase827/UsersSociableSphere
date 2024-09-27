b03c7f71e7757cdcf68e78f1b4feff30e1e98c6f
Peral Belenguer <aipg@gft.com>
2024-09-27 10:31:51 +0200

    Created UserDetailDTO and UserLoginDTO

---
+++ src/main/java/com/sociablesphere/usersociablesphere/api/dto/UserDetailDTO.java
@@ -0,0 +1,29 @@
+package com.sociablesphere.usersociablesphere.api.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailDTO {
+    private UUID id;
+    private String userName;
+    private String name;
+    private String lastName;
+    private String email;
+    private String photo;
+    private String description;
+    private String role;
+    private Double wallet;
+    private String apiToken;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
